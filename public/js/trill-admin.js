a47d45d41de2b1d64791da5d1e4fe456350eb025
SF55SF <2712055@gmail.com>
2026-07-30 00:55:53 +0500

    Fix admin office navigation

--- public/js/trill-admin.js
+++ public/js/trill-admin.js
@@ -31,3 +31,6 @@ finally{button.disabled=false;}
 });
 }
 /* TRILL_ADMIN_SAVE_CLIENT_FILE_END */
+
+/* TRILL_ADMIN_SWITCH_NAV */
+document.querySelectorAll("[data-office-list] a").forEach((link)=>{link.addEventListener("click",(event)=>{event.preventDefault();window.location.href=link.href;});});
